6f4d04151fb7cd71a3db268757e74c93b596c08e
Secret Keeper <shervin.ourfamily@gmail.com>
2022-02-14 12:56:35 +0330

    init

---
+++ src/message_repository.rs
@@ -0,0 +1,15 @@
+use crate::models::{CreateMessage, Message};
+use crate::schema::messages;
+use crate::schema::messages::dsl::*;
+use diesel;
+use diesel::prelude::*;
+
+pub fn create_message(message: CreateMessage, conn: &PgConnection) -> QueryResult<Message> {
+    diesel::insert_into(messages::table)
+        .values(&message)
+        .get_result(conn)
+}
+
+pub fn get_messages(connection: &PgConnection) -> QueryResult<Vec<Message>> {
+    messages.limit(5).load::<Message>(&*connection)
+}
