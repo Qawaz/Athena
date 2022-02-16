2f0c79f6c88690e617df23c1094c8cc65588f710
Secret Keeper <shervin.ourfamily@gmail.com>
2022-02-16 17:39:10 +0330

    removed unused println

--- src/main.rs
+++ src/main.rs
@@ -117,8 +117,6 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WsChatSession {
             Ok(msg) => msg,
         };
 
-        println!("WEBSOCKET MESSAGE: {:?}", msg);
-
         match msg {
             ws::Message::Ping(msg) => {
                 self.hb = Instant::now();
