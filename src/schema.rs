62aa1055c67b7b6c12ab8cc51d145f779060aebd
Secret Keeper <shervin.ourfamily@gmail.com>
2022-03-03 15:08:54 +0330

    created table! profiles schema

--- src/schema.rs
+++ src/schema.rs
@@ -9,3 +9,20 @@ table! {
         deleted_at -> Nullable<Timestamp>,
     }
 }
+
+table! {
+    profiles (id) {
+        id -> Int4,
+        user_id -> Int4,
+        status -> Nullable<Varchar>,
+        description -> Nullable<Varchar>,
+        created_at -> Timestamp,
+        updated_at -> Nullable<Timestamp>,
+        deleted_at -> Nullable<Timestamp>,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    messages,
+    profiles,
+);
