403e28a7527813f18e26b95512e552d223cd18e3
Secret Keeper <shervin.ourfamily@gmail.com>
2022-03-07 15:52:26 +0330

    added UserAPI & ProfileAPI struct

--- src/models/user.rs
+++ src/models/user.rs
@@ -9,9 +9,26 @@ pub struct User {
     pub ethereum_pubkey: Option<String>,
     pub username: Option<String>,
     pub email: Option<String>,
+    #[serde(skip_serializing)]
     pub password: Option<String>,
     pub avatar: Option<String>,
     pub created_at: NaiveDateTime,
+    #[serde(skip_serializing)]
     pub updated_at: Option<NaiveDateTime>,
+    #[serde(skip_serializing)]
     pub deleted_at: Option<NaiveDateTime>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct UserAPI {
+    #[serde(flatten)]
+    pub user: User,
+    #[serde(flatten)]
+    pub profile: ProfileAPI,
+}
+
+#[derive(Debug, Serialize, Queryable)]
+pub struct ProfileAPI {
+    pub status: Option<String>,
+    pub description: Option<String>,
 }
