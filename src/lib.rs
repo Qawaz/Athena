4d6249583409eae8d04feb998865a07b05ec7ebe
Secret Keeper <shervin.ourfamily@gmail.com>
2022-03-07 13:16:38 +0330

    code refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,11 @@
 extern crate diesel;
 extern crate dotenv;
 
+pub mod controllers;
 pub mod db;
 pub mod errors;
 pub mod extractors;
+pub mod message_handlers;
 pub mod message_repository;
 pub mod models;
 pub mod schema;
