a8f01d3af2eeedb501e08e3ab349fc83b5bf247b
Secret Keeper <shervin.ourfamily@gmail.com>
2022-03-07 15:53:44 +0330

    return UserAPI instead of User

--- src/message_handlers/user_handlers.rs
+++ src/message_handlers/user_handlers.rs
@@ -1,27 +1,35 @@
-use crate::db::DbExecutor;
 use crate::errors::ServiceError;
-use crate::models::user::User;
+use crate::models::profile::Profile;
+use crate::models::user::{ProfileAPI, User};
 use crate::models::user_requests::GetUserByIDReq;
+use crate::schema::profiles::dsl::*;
 use crate::schema::users::dsl::*;
+use crate::{db::DbExecutor, models::user::UserAPI};
 use actix::{Handler, Message, SyncContext};
 use diesel::prelude::*;
 
 impl Message for GetUserByIDReq {
-    type Result = Result<User, ServiceError>;
+    type Result = Result<UserAPI, ServiceError>;
 }
 
 impl Handler<GetUserByIDReq> for DbExecutor {
-    type Result = Result<User, ServiceError>;
+    type Result = Result<UserAPI, ServiceError>;
 
     fn handle(
         &mut self,
         user_id_request: GetUserByIDReq,
         _: &mut SyncContext<Self>,
     ) -> Self::Result {
-        let conn: &PgConnection = &self.1.get().unwrap();
+        let gateway_conn: &PgConnection = &self.1.get().unwrap();
+        let own_conn: &PgConnection = &self.0.get().unwrap();
 
-        let target_user = users.find(user_id_request.id).first(conn)?;
+        let user: User = users.find(user_id_request.id).first(gateway_conn)?;
+        let profile = Profile::belonging_to(&user)
+            .select((status, description))
+            .distinct()
+            .first::<ProfileAPI>(own_conn)
+            .unwrap();
 
-        Ok(target_user)
+        Ok(UserAPI { user, profile })
     }
 }
