c41acaca9515df47e23d72146c144f330d129d98
Secret Keeper <shervin.ourfamily@gmail.com>
2022-03-07 13:15:20 +0330

    init

---
+++ src/models/message.rs
@@ -0,0 +1,21 @@
+use crate::schema::messages;
+use diesel::{Insertable, Queryable};
+
+#[derive(Debug, Queryable, Insertable)]
+pub struct Message {
+    pub id: i32,
+    pub user_id: i32,
+    pub to_user_id: i32,
+    pub content: String,
+    pub created_at: chrono::NaiveDateTime,
+    pub updated_at: Option<chrono::NaiveDateTime>,
+    pub deleted_at: Option<chrono::NaiveDateTime>,
+}
+
+#[derive(Insertable)]
+#[table_name = "messages"]
+pub struct CreateMessage {
+    pub user_id: i32,
+    pub to_user_id: i32,
+    pub content: String,
+}
