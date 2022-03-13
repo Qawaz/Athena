e07c2af2fee97ef1de0ffe3266ca3eb57e81ff74
Secret Keeper <shervin.ourfamily@gmail.com>
2022-03-14 01:40:50 +0330

    add delivered to message schema

--- src/schema.rs
+++ src/schema.rs
@@ -28,6 +28,7 @@ table! {
         user_id -> Int4,
         to_user_id -> Int4,
         content -> Varchar,
+        delivered -> Bool,
         created_at -> Timestamp,
         updated_at -> Nullable<Timestamp>,
         deleted_at -> Nullable<Timestamp>,
