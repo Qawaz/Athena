2db3ba8161f0d7bbb72355f47b60343c784ce024
Secret Keeper <shervin.ourfamily@gmail.com>
2022-03-14 01:41:15 +0330

    add delivered column to messages table

--- migrations/2022-02-13-081716_create_messages/up.sql
+++ migrations/2022-02-13-081716_create_messages/up.sql
@@ -4,6 +4,7 @@ CREATE TABLE messages (
   user_id INTEGER NOT NULL,
   to_user_id INTEGER NOT NULL,
   content VARCHAR NOT NULL,
+  delivered BOOLEAN NOT NULL DEFAULT FALSE,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP,
   deleted_at TIMESTAMP
