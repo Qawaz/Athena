cd8ab297079d1831ada9198456c7ba9138aede1f
Secret Keeper <shervin.ourfamily@gmail.com>
2022-04-03 11:52:44 +0430

    init

---
+++ src/models/delivery_report.rs
@@ -0,0 +1,20 @@
+use actix::Message;
+use serde::{Deserialize, Serialize};
+
+#[derive(Message, Debug, Serialize, Deserialize)]
+#[rtype(result = "()")]
+pub struct DeliveryReport {
+    pub event: String,
+    pub data: DeliveryReportContent,
+}
+#[derive(Default, Debug, PartialEq, Serialize, Deserialize)]
+pub struct DeliveryReportContent {
+    pub sender: usize,
+    pub ids: Vec<i32>,
+}
+
+impl DeliveryReportContent {
+    pub fn set_sender_id_from_jwt(&mut self, sender_id: usize) {
+        self.sender = sender_id
+    }
+}
