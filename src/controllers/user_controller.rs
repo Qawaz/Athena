146578374a4ca0e39de803d34453b8961f8d3e90
Secret Keeper <shervin.ourfamily@gmail.com>
2022-04-09 19:41:18 +0430

    add get_multiple_users API method

--- src/controllers/user_controller.rs
+++ src/controllers/user_controller.rs
@@ -1,15 +1,32 @@
 use actix::Addr;
 use actix_web::{
     get,
-    web::{Data, Path},
+    web::{Data, Json, Path},
     HttpResponse, Responder, ResponseError,
 };
 
-use crate::{db::DbExecutor, errors::ServiceError, models::user_requests::GetUserByIDReq};
+use crate::{
+    db::DbExecutor,
+    errors::ServiceError,
+    models::user_requests::{GetMultipleUsers, GetUserByIDReq},
+};
 
 #[get("/id/{id}")]
 async fn get_user_by_id((id, addr): (Path<i32>, Data<Addr<DbExecutor>>)) -> impl Responder {
     let actix_message = addr.send(GetUserByIDReq { id: *id }).await;
+    let result = actix_message.unwrap();
+
+    match result {
+        Ok(response) => HttpResponse::Ok().json(response),
+        Err(error) => ServiceError::error_response(&error),
+    }
+}
+
+#[get("/multiple")]
+async fn get_multiple_users(
+    (users_ids, addr): (Json<GetMultipleUsers>, Data<Addr<DbExecutor>>),
+) -> impl Responder {
+    let actix_message = addr.send(users_ids.into_inner()).await;
     let result = actix_message.unwrap();
 
     match result {
