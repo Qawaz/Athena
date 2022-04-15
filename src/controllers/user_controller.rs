0626809368617a015d583a2da4336bad1fefd004
Secret Keeper <shervin.ourfamily@gmail.com>
2022-04-16 04:16:32 +0430

    add set_avatar method

--- src/controllers/user_controller.rs
+++ src/controllers/user_controller.rs
@@ -1,13 +1,19 @@
+use std::io::Write;
+
 use actix::Addr;
+use actix_multipart::Multipart;
 use actix_web::{
     get, post,
-    web::{Data, Json, Path},
-    HttpResponse, Responder, ResponseError,
+    web::{self, Data, Json, Path},
+    Error, HttpResponse, Responder, ResponseError,
 };
+use futures::TryStreamExt;
+use uuid::Uuid;
 
 use crate::{
     db::DbExecutor,
     errors::ServiceError,
+    extractors::jwt_data_decode::Auth,
     models::user_requests::{GetMultipleUsers, GetUserByIDReq},
 };
 
@@ -35,4 +41,54 @@ async fn get_multiple_users(
         Ok(response) => HttpResponse::Ok().json(response),
         Err(error) => ServiceError::error_response(&error),
     }
+}
+
+#[post("/user/set-avatar")]
+async fn set_avatar(
+    (mut form_data, addr, sub): (Multipart, Data<Addr<DbExecutor>>, Auth),
+) -> Result<HttpResponse, Error> {
+    while let Some(mut field) = form_data.try_next().await? {
+        // A multipart/form-data stream has to contain `content_disposition`
+        let content_disposition = field.content_disposition();
+
+        let filename = content_disposition
+            .get_filename()
+            .map_or_else(|| Uuid::new_v4().to_string(), sanitize_filename::sanitize);
+
+        let parts: Vec<&str> = filename.split('.').collect();
+
+        let res = match parts.last() {
+            Some(v) => match *v {
+                "png" => mime::IMAGE_PNG,
+                "jpg" => mime::IMAGE_JPEG,
+                &_ => mime::TEXT_PLAIN,
+            },
+            None => mime::TEXT_PLAIN,
+        };
+
+        println!("file extension is: {:?}", res.subtype());
+
+        println!("{:?}", filename);
+        let filepath = format!(
+            "./tmp/{}",
+            format!("{}.{}", sub.user_id.to_string(), res.subtype().to_owned())
+        );
+
+        // File::create is blocking operation, use threadpool
+        let mut f = web::block(|| std::fs::File::create(filepath)).await??;
+
+        // Field in turn is stream of *Bytes* object
+        while let Some(chunk) = field.try_next().await? {
+            // filesystem operations are blocking, we have to use threadpool
+            f = web::block(move || f.write_all(&chunk).map(|_| f)).await??;
+        }
+        // let actix_message = addr.send(GetUserByIDReq { id: *id }).await;
+        // let result = actix_message.unwrap();
+    }
+    Ok(HttpResponse::Ok().into())
+
+    // match result {
+    //     Ok(response) => HttpResponse::Ok().json(response),
+    //     Err(error) => ServiceError::error_response(&error),
+    // }
 }
