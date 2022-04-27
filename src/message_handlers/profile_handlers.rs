fde1bc143fbaca9dc52fb0e507a7dc43a815a277
Secret Keeper <shervin.ourfamily@gmail.com>
2022-04-27 18:30:58 +0430

    add actix handler for SetStatusRequest struct and implement update logics

--- src/message_handlers/profile_handlers.rs
+++ src/message_handlers/profile_handlers.rs
@@ -1,6 +1,6 @@
 use crate::db::DbExecutor;
 use crate::errors::ServiceError;
-use crate::models::profile::{GetUserProfile, Profile};
+use crate::models::profile::{GetUserProfile, Profile, SetStatusRequest};
 use crate::schema::profiles::dsl::*;
 use actix::{Handler, Message, SyncContext};
 use diesel::prelude::*;
@@ -24,5 +24,27 @@ impl Handler<GetUserProfile> for DbExecutor {
             .first(own_conn)?;
 
         Ok(profile)
+    }
+}
+
+impl Message for SetStatusRequest {
+    type Result = Result<usize, ServiceError>;
+}
+
+impl Handler<SetStatusRequest> for DbExecutor {
+    type Result = Result<usize, ServiceError>;
+
+    fn handle(
+        &mut self,
+        set_status_request: SetStatusRequest,
+        _: &mut SyncContext<Self>,
+    ) -> Self::Result {
+        let own_conn: &PgConnection = &self.0.get().unwrap();
+
+        let update_status = diesel::update(profiles.filter(user_id.eq(set_status_request.sender)))
+            .set(status.eq(set_status_request.status))
+            .execute(own_conn)?;
+
+        Ok(update_status)
     }
 }
