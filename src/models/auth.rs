2b2d6ea21453d12e3fc482dbfb7fa3948ce3efee
Secret Keeper <shervin.ourfamily@gmail.com>
2022-05-24 04:38:26 +0430

    init

---
+++ src/models/auth.rs
@@ -0,0 +1,16 @@
+use serde::{Deserialize, Serialize};
+use super::user::UserResponse;
+
+#[derive(Debug, Deserialize)]
+pub struct LoginRequest {
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Debug, Serialize)]
+pub struct LoginResponse {
+    pub access_token: String,
+    pub refresh_token: String,
+    pub expires: String,
+    pub user: UserResponse,
+}
